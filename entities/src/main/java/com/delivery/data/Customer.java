c85ee250411f9877509df7488c17669745638b0b
chipallino1 <skorupich00@mail.ru>
2021-02-09 22:22:37 +0300

    Added Customer entity.

---
+++ entities/src/main/java/com/delivery/data/Customer.java
@@ -0,0 +1,4 @@
+package com.delivery.data;
+
+public class Customer {
+}
--- services/src/main/java/com/delivery/dto/UserDto.java
+++ services/src/main/java/com/delivery/dto/UserDto.java
@@ -2,11 +2,13 @@ package com.delivery.dto;
 
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.Date;
 import java.util.UUID;
 
 @Data
-public class UserDto {
+public class UserDto implements Serializable {
+    private static final long serialVersionUID = 6727229656276658113L;
     private UUID id;
     private String userName;
     private Date birthDate;
